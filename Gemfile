03ca365dfc068c84d6b76d798602965c30ebb4e7
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2014-06-06 19:29:55 +0300

    #TB-40 Middleman updates and improvements - Middleman gems were updated - HAML layout structures were updated - Bootstrap-sass was updated - Middleman-deploy plugin was enabled and rakefile was removed - .gitignore file was updated

--- Gemfile
+++ Gemfile
@@ -2,19 +2,25 @@
 # the following line to use "https"
 source 'http://rubygems.org'
 
-gem 'middleman', '~>3.2.0'
+gem 'middleman', '~>3.3.3'
 
 # Live-reloading plugin
 gem 'middleman-livereload', '~> 3.1.0'
 
+# Middleman-sprockets
 gem 'middleman-sprockets', '3.3.2'
+
+# Middleman-deploy
+gem 'middleman-deploy', '~> 0.2.4'
 
 # For faster file watcher updates on Windows:
 gem 'wdm', '~> 0.1.0', :platforms => [:mswin, :mingw]
 
-# Compass
-gem 'compass', '~> 0.12.2'
+# Windows does not come with time zone data
+gem 'tzinfo-data', platforms: [:mswin, :mingw]
 
-# Bootstrap sass
-gem 'bootstrap-sass', '~> 3.0.3.0'
-gem 'middleman-gh-pages'
+# Compass
+gem 'compass', '~> 0.12.6'
+
+# Twitter Bootstrap
+gem 'bootstrap-sass', '~> 3.1.1.1'
--- config.rb
+++ config.rb
@@ -2,50 +2,32 @@
 # Compass
 ###
 
-# Bootstrap
-# First: 
-#    gem install sass-rails
-#    gem install bootstrap-sass
-require 'bootstrap-sass'
-
-# Susy grids in Compass
-# First: gem install compass-susy-plugin
-# require 'susy'
-
 # Change Compass configuration
-compass_config do |config|
-  config.output_style = :nested
-  config.http_path = '/'
-  config.images_dir = 'assets/images'
-  config.javascripts_dir = 'assets/javascripts'
-end
-
-# Cache Buster
-#activate :asset_hash
-activate :cache_buster
+# compass_config do |config|
+#   config.output_style = :compact
+# end
 
 ###
 # Page options, layouts, aliases and proxies
 ###
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
-# A path which all have the same  layout
+#
+# A path which all have the same layout
 with_layout :login do
-   page '/index.html'
-   page '/forgot_password.html'
+  page '/index.html'
+  page '/forgot_password.html'
 end
 
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
@@ -54,8 +36,13 @@ end
 # Automatic image dimensions on image_tag helper
 activate :automatic_image_sizes
 
-# Live Reload
-activate :livereload
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+# Pretty URLs
+activate :directory_indexes
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -70,8 +57,9 @@ activate :i18n
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
 set :images_dir, 'assets/images'
+set :fonts_dir, 'assets/fonts'
 
-activate :directory_indexes
+set :haml, format: :html5
 
 # Build-specific configuration
 configure :build do
@@ -82,16 +70,20 @@ configure :build do
   activate :minify_javascript
 
   # Enable cache buster
-  activate :cache_buster
+  activate :asset_hash
 
   # Use relative URLs
   activate :relative_assets
 
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
   # Or use a different image path
-  #set :http_path, "/img/"
+  # set :http_prefix, "/Content/images/"
+end
+config[:file_watcher_ignore] += [ /.idea\// ]
+
+# Deployment
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  deploy.remote = 'git@github.com:lab2023/hierapolis.git'
+  deploy.branch = 'gh-pages'
 end
